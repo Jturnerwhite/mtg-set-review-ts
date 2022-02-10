02a89aef9da64051deca5933be06f08f863ad2bb
crystal-fecteau <crystal@abe.ai>
2022-02-10 10:11:16 -0800

    Incorporated Scryfall api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 import MainPage from './Components/test/MainPage';
 import PreviewPage from './Components/test/PreviewPage';
+import CardViewPage from './Components/cardView/CardViewPage';
 
 
 function App() {
@@ -17,10 +18,12 @@ function App() {
         <nav>
           <button><Link to='/'>Main</Link></button>
           <button><Link to='/page2'>Preview</Link></button>
+          <button><Link to='/cardView'>View Card</Link></button>
         </nav>
         <Routes>
           <Route path='/' element={<MainPage/>}/>
           <Route path='/page2' element={<PreviewPage/>}/>
+          <Route path='/cardView' element={<CardViewPage/>}/>
         </Routes>
         <p>Do Things!</p>
       </div>
