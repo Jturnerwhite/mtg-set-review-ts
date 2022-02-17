77a2e96685b9e5550fed738f72dd5a8dc3ad8d63
Jordan White <jturnerwhite@gmail.com>
2022-02-17 16:38:08 -0600

    Fixing redux connection typing issues

--- frontend/src/Components/cardView/CardViewPage.tsx
+++ frontend/src/Components/cardView/CardViewPage.tsx
@@ -1,73 +1,70 @@
+import { Actions } from "../../Store/Actions/Session.actions";
+import { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
-import { Actions } from "../../Store/Actions/Session.actions";
-import { useState } from 'react';
-import { SessionState } from "../../Store/States/Session.state";
-import { CardData } from "../../Interfaces/CardData";
 import { StateStructure } from "../../Store/store";
+import { CardData } from "../../Interfaces/CardData";
+import {
+  Props as DefaultProperties,
+  defaultMapStateToProps,
+  defaultMapDispatchToProps,
+} from "../../Interfaces/DefaultConnections";
 
-const CardViewPage = (props: any) => {
-    const sessionState = props.state.session;
-    // const dispatch = props.dispatch;
-    const firstThreeArray = sessionState.cards?.slice(0, 3) ?? [];
-    let [cardRating, setCardRating] = useState(0);
-    let [activeCard, setActiveCard] = useState(firstThreeArray[0]);
-    // let [nextCard, setNextCard] = useState();
-    // let [perviousCard, setPerviousCard] = useState()
-    
-    
-    //get first card and check if it has been rated
-    //if not rated, display card and get rating
-    //if rated move to next card
+const CardViewPage = (props: DefaultProperties) => {
+  const sessionState = props.state.session;
+  const dispatch = props.dispatch;
+  const firstThreeArray = sessionState.cards?.slice(0, 3) ?? [];
+  let [cardRating, setCardRating] = useState(0);
+  let [activeCard, setActiveCard] = useState(firstThreeArray[0]);
 
-    const setRating = () => {
-        dispatch(Actions.SetCardRating({
-            id: activeCard.id,
-            image: activeCard.image,
-            cardName: activeCard.cardName,
-            rating: cardRating
-        } as CardData))
-        firstThreeArray.forEach((card:CardData, index: number) => {
-            if(card.id == activeCard.id){
-                if(firstThreeArray.length -1 == index){
-                    //finish page here
-                }else{
-                    setActiveCard(firstThreeArray[index +1])
-                }
-            }
-        })
-    }
+  const setRating = () => {
+    dispatch(
+      Actions.SetCardRating({
+        id: activeCard.id,
+        image: activeCard.image,
+        cardName: activeCard.cardName,
+        rating: cardRating,
+      } as CardData)
+    );
+    firstThreeArray.forEach((card: CardData, index: number) => {
+      if (card.id == activeCard.id) {
+        if (firstThreeArray.length - 1 == index) {
+          //finish page here
+        } else {
+          setActiveCard(firstThreeArray[index + 1]);
+        }
+      }
+    });
+  };
 
-    return(
-        <>
-            <h1>number of cards: {props.state.session.cards?.length}</h1>
-            <select onChange = {(event) => setCardRating(parseInt(event.target.value)) }>
-                <option value='0'>0</option>
-                <option value='1'>1</option>
-                <option value='2'>2</option>
-                <option value='3'>3</option>
-                <option value='4'>4</option>
-                <option value='5'>5</option>
-                <option value='6'>6</option>
-                <option value='7'>7</option>
-                <option value='8'>8</option>
-                <option value='9'>9</option>
-                <option value='10'>10</option>
-            </select>
-            <button onClick={setRating} type='submit'>Submit</button>
-            <div>
-                <p>{activeCard.cardName}</p>
-                <img src={activeCard.image}/>
-            </div>
-        </>
-    ) 
-}
-
-const defaultMapStateToProps = (state: StateStructure)=> {
-    return { state: state};
+  return (
+    <>
+      <h1>number of cards: {props.state.session.cards?.length}</h1>
+      <select onChange={(event) => setCardRating(parseInt(event.target.value))}>
+        <option value="0">0</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </select>
+      <button onClick={setRating} type="submit">
+        Submit
+      </button>
+      <div>
+        <p>{activeCard.cardName}</p>
+        <img src={activeCard.image} />
+      </div>
+    </>
+  );
 };
-const defaultMapDispatchToProps = (dispatch: Dispatch<any>) => {
-    return { dispatch: dispatch };
-};
-  
-export default connect(defaultMapStateToProps, defaultMapDispatchToProps) (CardViewPage);
+
+export default connect(
+  defaultMapStateToProps,
+  defaultMapDispatchToProps
+)(CardViewPage);
--- frontend/src/Components/sessionView/SessionViewPage.tsx
+++ frontend/src/Components/sessionView/SessionViewPage.tsx
@@ -1,94 +1,111 @@
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import { SessionState } from "../../Store/States/Session.state";
-import { useNavigate } from 'react-router-dom'
-import { Actions } from '../../Store/Actions/Session.actions';
+import { useNavigate } from "react-router-dom";
+import { Actions } from "../../Store/Actions/Session.actions";
 import { useEffect, useState } from "react";
-import { SetData } from '../../Interfaces/SetData';
+import { SetData } from "../../Interfaces/SetData";
 import { CardData } from "../../Interfaces/CardData";
 import { StateStructure } from "../../Store/store";
+import Action from "../../Store/action.interface";
+import {
+  Props as DefaultProperties,
+  defaultMapStateToProps,
+  defaultMapDispatchToProps,
+} from "../../Interfaces/DefaultConnections";
 
+const SessionViewPage = (props: DefaultProperties) => {
+  const sessionState = props.state.session;
+  const dispatch = props.dispatch;
+  let navigate = useNavigate();
+  let [name, setName] = useState("");
+  let [sets, cardSets] = useState([] as Array<SetData>);
+  let [setId, setCardId] = useState(0);
 
-const SessionViewPage = (props: {
-    state: StateStructure,
-    dispatch: Dispatch<any>
-}& {}) => {
-    const sessionState = props.state.session;
-    const dispatch = props.dispatch;
-    let navigate = useNavigate();
-    let [name, setName] = useState('')
-    let [sets, cardSets] = useState([] as Array<SetData>);
-    let [setId, setCardId] = useState(0);
-
-    useEffect(() => {
-        if (sets.length == 0) {
-            fetch("https://api.scryfall.com/sets")
-            .then(res => res.json())
-            .then(json => {
-                let mappedSets: Array<SetData> = json.data.filter((set:any)=> {
-                    return (set.set_type === "expansion" || set.set_type === "core") && set.card_count > 0
-                }).map((singleSet: any) => {
-                    return({
-                        id: singleSet.id,
-                        name: singleSet.name,
-                        code: singleSet.code,
-                    } as SetData);
-                });
-                cardSets(mappedSets)
+  useEffect(() => {
+    if (sets.length == 0) {
+      fetch("https://api.scryfall.com/sets")
+        .then((res) => res.json())
+        .then((json) => {
+          let mappedSets: Array<SetData> = json.data
+            .filter((set: any) => {
+              return (
+                (set.set_type === "expansion" || set.set_type === "core") &&
+                set.card_count > 0
+              );
+            })
+            .map((singleSet: any) => {
+              return {
+                id: singleSet.id,
+                name: singleSet.name,
+                code: singleSet.code,
+              } as SetData;
             });
-        }
-    },[])
-   async function getCards(endPoint: string): Promise<Array<CardData>> {
-       let response = await fetch(endPoint).then( res => res.json());
-       let cards: Array<CardData> = response.data.map((card: any) => {
-            return({
-                id: card.id,
-                image: card.image_uris?.normal ?? '',
-                cardName: card.name,
-            } as CardData)
-       })
-       if (response.has_more) {
-           let nextPageCards = await getCards(response.next_page);
-           return [...cards, ...nextPageCards] ;
-       }
-       return cards;
+          cardSets(mappedSets);
+        });
     }
-    
-    
-    async function setSession () {
-        let selectedSet = sets[setId];
-        let allCards =  await getCards(`https://api.scryfall.com/cards/search?order=set&q=set%3A${selectedSet.code}&unique=cards`);
-
-        dispatch(Actions.SetSession({
-            name: name,
-            id: new Date().valueOf().toString(),
-            cardSet: selectedSet,
-            cards: allCards
-        } as SessionState))
-
-        navigate('/cardView');
+  }, []);
+  async function getCards(endPoint: string): Promise<Array<CardData>> {
+    let response = await fetch(endPoint).then((res) => res.json());
+    let cards: Array<CardData> = response.data.map((card: any) => {
+      return {
+        id: card.id,
+        image: card.image_uris?.normal ?? "",
+        cardName: card.name,
+      } as CardData;
+    });
+    if (response.has_more) {
+      let nextPageCards = await getCards(response.next_page);
+      return [...cards, ...nextPageCards];
     }
+    return cards;
+  }
 
-    return (
-        <div>
-            <form id='sessionForm'>
-                <label htmlFor='sessionName'>Session Name: {sessionState.name} </label>
-                <input type='text' id='sessionName' name='sessionName' onChange={(event) => setName(event.target.value)}></input>
-                <select onChange={(event) => setCardId(parseInt(event.target.value))}>
-                    {sets.map((set, index) => <option key={index} value={index}>{set.name}</option>)}
-                </select>
-            </form>
-            <button onClick={setSession} type='submit'>Submit</button>
-            <button>Cancel</button>
-        </div>
-    )
-}
+  async function setSession() {
+    let selectedSet = sets[setId];
+    let allCards = await getCards(
+      `https://api.scryfall.com/cards/search?order=set&q=set%3A${selectedSet.code}&unique=cards`
+    );
 
-const defaultMapStateToProps = (state: StateStructure): any => {
-    return { state: state };
+    dispatch(
+      Actions.SetSession({
+        name: name,
+        id: new Date().valueOf().toString(),
+        cardSet: selectedSet,
+        cards: allCards,
+      } as SessionState)
+    );
+
+    navigate("/cardView");
+  }
+
+  return (
+    <div>
+      <form id="sessionForm">
+        <label htmlFor="sessionName">Session Name: {sessionState.name} </label>
+        <input
+          type="text"
+          id="sessionName"
+          name="sessionName"
+          onChange={(event) => setName(event.target.value)}
+        ></input>
+        <select onChange={(event) => setCardId(parseInt(event.target.value))}>
+          {sets.map((set, index) => (
+            <option key={index} value={index}>
+              {set.name}
+            </option>
+          ))}
+        </select>
+      </form>
+      <button onClick={setSession} type="submit">
+        Submit
+      </button>
+      <button>Cancel</button>
+    </div>
+  );
 };
-const defaultMapDispatchToProps = (dispatch: Dispatch<any>): any => {
-    return { dispatch: dispatch };
-};
 
-export default connect(defaultMapStateToProps, defaultMapDispatchToProps) (SessionViewPage);
+export default connect(
+  defaultMapStateToProps,
+  defaultMapDispatchToProps
+)(SessionViewPage);
